0ad4bf2c698f1f8d3c047dd62d1b8e06a23e0360
Thibault NORMAND <thibault.normand@sogeti.com>
2017-02-10 08:33:57 +0100

    [APP] Add moods.

--- domain/mood.go
+++ domain/mood.go
@@ -6,15 +6,19 @@ type Mood uint64
 const (
 	// UndefinedMood indicates that the user has not voted
 	UndefinedMood Mood = iota // 0
+	// AngryMood indicates tha user is angry during the voting day
+	AngryMood // 1
 	// BadMood indicates that user have bad feelings during the voting day
-	BadMood // 1
+	BadMood // 2
 	// ModerateMood indicates that user has moderate feelings during voting day
-	ModerateMood // 2
+	ModerateMood // 3
 	// GoodMood indicates that user has good feelings during voting day
-	GoodMood // 3
+	GoodMood // 4
+	// HappyMood idicates that the user is happy during the voting day
+	HappyMood // 5
 )
 
-var moods = []string{"undefined", "bad", "moderate", "good"}
+var moods = []string{"undefined", "angry", "bad", "moderate", "good", "happy"}
 
 // String returns string representation of the given enumeration
 func (m Mood) String() string {
--- dto/mapper/mood.go
+++ dto/mapper/mood.go
@@ -5,16 +5,30 @@ import (
 	"zenithar.org/go/nikoniko/dto"
 )
 
+var (
+	moodMap = map[domain.Mood]dto.Domain_Mood{
+		domain.AngryMood:    dto.Domain_Angry,
+		domain.BadMood:      dto.Domain_Bad,
+		domain.ModerateMood: dto.Domain_Moderate,
+		domain.GoodMood:     dto.Domain_Good,
+		domain.HappyMood:    dto.Domain_Happy,
+	}
+)
+
 // FromMood converts a domain mood to dto one
 func FromMood(entity domain.Mood) dto.Domain_Mood {
-	switch entity {
-	case domain.BadMood:
-		return dto.Domain_Bad
-	case domain.ModerateMood:
-		return dto.Domain_Moderate
-	case domain.GoodMood:
-		return dto.Domain_Good
-	default:
-		return dto.Domain_Undefined
+	if value, ok := moodMap[entity]; ok {
+		return value
 	}
+	return dto.Domain_Undefined
+}
+
+// ToMood converts a dto mood to domain one
+func ToMood(object dto.Domain_Mood) domain.Mood {
+	for k, v := range moodMap {
+		if v == object {
+			return k
+		}
+	}
+	return domain.UndefinedMood
 }
